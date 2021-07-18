f963aed1249459e2197d236eb77369ac46b04dd6
Nandraj <nandraj.rathod.009@gmail.com>
2021-07-18 18:29:49 +0530

    fileName changed to jsx for prettier formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
-import './App.css';
+import './App.css'
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <div className="App">
         <h1>Hello from React</h1>
       </div>
-    )}
+    )
+  }
 }
 
-export default App;
+export default App
