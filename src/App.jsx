07479e4a338b96608125f8cb16b5d678c6fb3844
Nandraj <nandraj.rathod.009@gmail.com>
2021-07-19 12:49:38 +0530

    basic of class based component. userItem list done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello from React</h1>
+      <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
