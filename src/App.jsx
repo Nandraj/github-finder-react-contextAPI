aa97ebe2c8aedfc7ddf050d20415e18cca6ff205
Nandraj <nandraj.rathod.009@gmail.com>
2021-07-21 16:25:56 +0530

    search n Users wrapped into Home. NotFound Pg add.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
+import Home from './components/Pages/Home';
 import Alert from './components/layout/Alert';
 import About from './components/Pages/About';
 import User from './components/users/User';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import NotFound from './components/Pages/NotFound';
 import './App.css';
 
 const App = () => {
@@ -20,18 +20,10 @@ const App = () => {
             <div className='container'>
               <Alert alert={alert} />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
